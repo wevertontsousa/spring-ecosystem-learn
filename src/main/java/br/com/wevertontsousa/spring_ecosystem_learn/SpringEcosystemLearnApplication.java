c285c5beb7c1b221c7736512b3bacdf2b6500aaa
Weverton Sousa <weverton.tsousa@gmail.com>
2024-10-08 14:36:24 +0000

    :tada: iniciar um projeto Maven com Spring Initializr

---
+++ src/main/java/br/com/wevertontsousa/spring_ecosystem_learn/SpringEcosystemLearnApplication.java
@@ -0,0 +1,13 @@
+package br.com.wevertontsousa.spring_ecosystem_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcosystemLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcosystemLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wevertontsousa/spring_ecosystem_learn/SpringEcosystemLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wevertontsousa.spring_ecosystem_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcosystemLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
